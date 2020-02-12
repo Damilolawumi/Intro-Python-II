97f2f5d1886cdb5a1c93d389fd81f933bc416f73
Megan Ennis <Megan@Megans-iMac-9.local>
2020-02-12 17:50:26 +0000

    Add inventory attribute and methods to grab and drop items to player class

--- src/player.py
+++ src/player.py
@@ -6,5 +6,13 @@ class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room  
+        self.inventory = []
+
     def __str__(self):
-        return "name: {}, current room: {}".format(self.name, self.current_room)
+        return "name: {}, current room: {}, inventory: {}".format(self.name, self.current_room, self.inventory)
+
+    def grab_item(self, item):
+        self.inventory.append(item)    
+
+    def drop_item(self, item):
+        self.inventory.remove(item)    
