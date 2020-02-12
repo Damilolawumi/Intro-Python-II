a9eec39bea52ad5e9799c4d19da9e9689a13266b
Megan Ennis <Megan@Megans-iMac-9.local>
2020-02-12 17:51:58 +0000

    Add items list attribute and methods to add and remove items to room class

--- src/room.py
+++ src/room.py
@@ -11,6 +11,13 @@ class Room:
         self.s_to = None
         self.e_to = None
         self.w_to = None
+        self.items = []
 
     def __str__(self):
-        return "name: {}, description: {}".format(self.name, self.description)    
+        return "name: {}, description: {}, items: {}".format(self.name, self.description, self.items)    
+
+    def add_item(self, item):
+        self.items.append(item)    
+
+    def remove_item(self, item):
+        self.items.remove(item)    
