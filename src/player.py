7b1854d543923fdc76791f30a9ad3a12b7c77003
Megan Ennis <Megan@Megans-iMac-9.local>
2020-02-13 16:15:44 +0000

    Add score attribute to player class. Initialise to 0

--- src/player.py
+++ src/player.py
@@ -7,9 +7,10 @@ class Player:
         self.name = name
         self.current_room = current_room  
         self.inventory = []
+        self.score = 0
 
     def __str__(self):
-        return "name: {}, current room: {}, inventory: {}".format(self.name, self.current_room, self.inventory)
+        return "name: {}, current room: {}, inventory: {}, score: {}".format(self.name, self.current_room, self.inventory, self.score)
 
     def grab_item(self, item):
         self.inventory.append(item)    
